1a904f83e19f6fd7d3f1d29abc2da284f8d95f8f
JustinSQiu <justinsunqiu@gmail.com>
2024-11-08 03:15:36 -0500

    all

--- STEL_probing.py
+++ STEL_probing.py
@@ -1,67 +1,108 @@
-from datadreamer import DataDreamer
-from datadreamer.llms import OpenAI
-from datadreamer.steps import DataFromPrompt, Embed, CosineSimilarity, concat, HFHubDataSource
-from datadreamer.embedders import SentenceTransformersEmbedder
-import numpy as np
 import math
+
+import numpy as np
 import pandas as pd
+from datadreamer import DataDreamer
+from datadreamer.embedders import Embedder, SentenceTransformersEmbedder
+from datadreamer.llms import OpenAI
+from datadreamer.steps import (CosineSimilarity, DataFromPrompt, Embed,
+                               HFHubDataSource, concat)
+from transformers import AutoModel, AutoTokenizer
+
+from luar_utils import load_luar_as_sentence_transformer
+from stylegenome_lisa_sfam.lisa_inference_utils import (load_lisa,
+                                                        predict_lisa_embedder)
 
 NUM_ROWS_PER_CATEGORY = 100
 
 with DataDreamer('./output'):
     stel_dataset = HFHubDataSource(
-        'Lexical Features',
-        path='justinsunqiu/multilingual_stel_test',
-        split='data'
+        'Lexical Features', path='justinsunqiu/multilingual_stel', split='data'
     )
 
 
-def compute_embeddings(
-        dataset_pos, dataset_neg, model: str
-):
+def compute_embeddings(dataset_pos, dataset_neg, model_name, model):
     with DataDreamer('./output'):
         pos_embedded_data = Embed(
-            name = f'{model.replace("/", " ")} Embeddings for Positive Examples',
-            inputs = {
-                'texts': dataset_pos
+            name=f'{model_name.replace("/", " ")} Embeddings for Positive Examples',
+            inputs={'texts': dataset_pos},
+            args={
+                'embedder': model,
+                'truncate': True,
             },
-            args = {
-                'embedder': SentenceTransformersEmbedder(
-                    model_name=model
-                ),
-                'truncate': True
-            },
-            outputs = {
-                'texts': 'sentences',
-                'embeddings': 'embeddings'
-            },
+            outputs={'texts': 'sentences', 'embeddings': 'embeddings'},
         )
         neg_embedded_data = Embed(
-            name = f'{model.replace("/", " ")} Embeddings for Negative Examples',
-            inputs = {
-                'texts': dataset_neg
+            name=f'{model_name.replace("/", " ")} Embeddings for Negative Examples',
+            inputs={'texts': dataset_neg},
+            args={
+                'embedder': model,
+                'truncate': True,
             },
-            args = {
-                'embedder': SentenceTransformersEmbedder(
-                    model_name=model
-                ),
-                'truncate': True
-            },
-            outputs = {
-                'texts': 'sentences',
-                'embeddings': 'embeddings'
-            },
+            outputs={'texts': 'sentences', 'embeddings': 'embeddings'},
         )
     return pos_embedded_data, neg_embedded_data
+
 
 def convert_embeddings(pos_embedded_data, neg_embedded_data):
     paired_embeddings = []
     for i in range(len(pos_embedded_data.output) // NUM_ROWS_PER_CATEGORY):
-        pos_embeddings = np.array(pos_embedded_data.output['embeddings'][i * NUM_ROWS_PER_CATEGORY : (i+1) * NUM_ROWS_PER_CATEGORY])
-        neg_embeddings = np.array(neg_embedded_data.output['embeddings'][i * NUM_ROWS_PER_CATEGORY : (i+1) * NUM_ROWS_PER_CATEGORY])
+        pos_embeddings = np.array(
+            pos_embedded_data.output['embeddings'][
+                i * NUM_ROWS_PER_CATEGORY : (i + 1) * NUM_ROWS_PER_CATEGORY
+            ]
+        )
+        neg_embeddings = np.array(
+            neg_embedded_data.output['embeddings'][
+                i * NUM_ROWS_PER_CATEGORY : (i + 1) * NUM_ROWS_PER_CATEGORY
+            ]
+        )
         paired = [(pos, neg) for pos, neg in zip(pos_embeddings, neg_embeddings)]
         paired_embeddings.append(paired)
     return paired_embeddings
+
+
+def get_embeddings_LISA(dataset_pos, dataset_neg):
+    paired_embeddings = []
+    model, tokenizer, embedder = load_lisa('stylegenome_lisa_sfam/lisa_checkpoint')
+    for i in range(len(dataset_pos) // NUM_ROWS_PER_CATEGORY):
+        pos_embeddings = [
+            predict_lisa_embedder(model, tokenizer, embedder, pos_text)
+            for pos_text in dataset_pos[
+                i * NUM_ROWS_PER_CATEGORY : (i + 1) * NUM_ROWS_PER_CATEGORY
+            ]
+        ]
+        neg_embeddings = [
+            predict_lisa_embedder(model, tokenizer, embedder, neg_text)
+            for neg_text in dataset_neg[
+                i * NUM_ROWS_PER_CATEGORY : (i + 1) * NUM_ROWS_PER_CATEGORY
+            ]
+        ]
+        paired = [(pos, neg) for pos, neg in zip(pos_embeddings, neg_embeddings)]
+        paired_embeddings.append(tuple(paired))
+    return paired_embeddings
+
+
+def get_embeddings_LUAR(dataset_pos, dataset_neg):
+    paired_embeddings = []
+    model = load_luar_as_sentence_transformer('rrivera1849/LUAR-MUD')
+    for i in range(len(dataset_pos) // NUM_ROWS_PER_CATEGORY):
+        pos_embeddings = [
+            model.encode(pos_text)
+            for pos_text in dataset_pos[
+                i * NUM_ROWS_PER_CATEGORY : (i + 1) * NUM_ROWS_PER_CATEGORY
+            ]
+        ]
+        neg_embeddings = [
+            model.encode(neg_text)
+            for neg_text in dataset_neg[
+                i * NUM_ROWS_PER_CATEGORY : (i + 1) * NUM_ROWS_PER_CATEGORY
+            ]
+        ]
+        paired = [(pos, neg) for pos, neg in zip(pos_embeddings, neg_embeddings)]
+        paired_embeddings.append(tuple(paired))
+    return paired_embeddings
+
 
 def compute_accuracy_STEL(paired_embeddings: list):
     accuracy = 0
@@ -70,24 +111,33 @@ def compute_accuracy_STEL(paired_embeddings: list):
     incorrect = 0
     for i in range(len(paired_embeddings)):
         anchor_pos, anchor_neg = paired_embeddings[i]
-        norm_anchor_pos, norm_anchor_neg = anchor_pos / np.linalg.norm(anchor_pos), anchor_neg / np.linalg.norm(anchor_neg)
-        for j in range(i+1, len(paired_embeddings)):
+        norm_anchor_pos, norm_anchor_neg = anchor_pos / np.linalg.norm(
+            anchor_pos
+        ), anchor_neg / np.linalg.norm(anchor_neg)
+        for j in range(i + 1, len(paired_embeddings)):
             alt_pos, alt_neg = paired_embeddings[j]
-            norm_alt_pos, norm_alt_neg = alt_pos / np.linalg.norm(alt_pos), alt_neg / np.linalg.norm(alt_neg)
+            norm_alt_pos, norm_alt_neg = alt_pos / np.linalg.norm(
+                alt_pos
+            ), alt_neg / np.linalg.norm(alt_neg)
             sim1 = np.dot(norm_anchor_pos, norm_alt_pos)
             sim2 = np.dot(norm_anchor_neg, norm_alt_neg)
             sim3 = np.dot(norm_anchor_pos, norm_alt_neg)
             sim4 = np.dot(norm_anchor_neg, norm_alt_pos)
-            if math.pow(1 - sim1, 2) + math.pow(1 - sim2, 2) == math.pow(1 - sim3, 2) + math.pow(1 - sim4, 2):
+            if math.pow(1 - sim1, 2) + math.pow(1 - sim2, 2) == math.pow(
+                1 - sim3, 2
+            ) + math.pow(1 - sim4, 2):
                 accuracy += 0.5
                 rand += 1
-            elif math.pow(1 - sim1, 2) + math.pow(1 - sim2, 2) < math.pow(1 - sim3, 2) + math.pow(1 - sim4, 2):
+            elif math.pow(1 - sim1, 2) + math.pow(1 - sim2, 2) < math.pow(
+                1 - sim3, 2
+            ) + math.pow(1 - sim4, 2):
                 accuracy += 1
                 correct += 1
             else:
                 accuracy += 0
                 incorrect += 1
     return accuracy / (len(paired_embeddings) * (len(paired_embeddings) - 1) / 2)
+
 
 def compute_accuracy_STEL_or_content(paired_embeddings: list):
     accuracy = 0
@@ -96,10 +146,14 @@ def compute_accuracy_STEL_or_content(paired_embeddings: list):
     incorrect = 0
     for i in range(len(paired_embeddings)):
         anchor_pos, anchor_neg = paired_embeddings[i]
-        norm_anchor_pos, norm_anchor_neg = anchor_pos / np.linalg.norm(anchor_pos), anchor_neg / np.linalg.norm(anchor_neg)
-        for j in range(i+1, len(paired_embeddings)):
+        norm_anchor_pos, norm_anchor_neg = anchor_pos / np.linalg.norm(
+            anchor_pos
+        ), anchor_neg / np.linalg.norm(anchor_neg)
+        for j in range(i + 1, len(paired_embeddings)):
             alt_pos, alt_neg = paired_embeddings[j]
-            norm_alt_pos, norm_alt_neg = alt_pos / np.linalg.norm(alt_pos), alt_neg / np.linalg.norm(alt_neg)
+            norm_alt_pos, norm_alt_neg = alt_pos / np.linalg.norm(
+                alt_pos
+            ), alt_neg / np.linalg.norm(alt_neg)
             norm_alt_neg = norm_anchor_neg
             sim1 = np.dot(norm_anchor_pos, norm_alt_pos)
             sim2 = np.dot(norm_anchor_pos, norm_alt_neg)
@@ -114,9 +168,17 @@ def compute_accuracy_STEL_or_content(paired_embeddings: list):
                 incorrect += 1
     return accuracy / (len(paired_embeddings) * (len(paired_embeddings) - 1) / 2)
 
-def STEL_benchmark(dataset_pos, dataset_neg, model, type='STEL'):
-    pos_embedded_data, neg_embedded_data = compute_embeddings(dataset_pos, dataset_neg, model)
-    paired_embeddings = convert_embeddings(pos_embedded_data, neg_embedded_data)
+
+def STEL_benchmark(dataset_pos, dataset_neg, model_name, model, type='STEL'):
+    if model_name == 'lisa':
+        paired_embeddings = get_embeddings_LISA(dataset_pos, dataset_neg)
+    elif model_name == 'luar':
+        paired_embeddings = get_embeddings_LUAR(dataset_pos, dataset_neg)
+    else:
+        pos_embedded_data, neg_embedded_data = compute_embeddings(
+            dataset_pos, dataset_neg, model_name, model
+        )
+        paired_embeddings = convert_embeddings(pos_embedded_data, neg_embedded_data)
     accuracies = []
     for paired in paired_embeddings:
         if type == 'STEL':
@@ -125,25 +187,37 @@ def STEL_benchmark(dataset_pos, dataset_neg, model, type='STEL'):
             accuracies.append(compute_accuracy_STEL_or_content(paired))
     avg_accuracy = np.mean(accuracies)
     return accuracies, avg_accuracy
-    
+
 
 def STEL_categories():
     categories = []
     for i in range(len(stel_dataset.output) // NUM_ROWS_PER_CATEGORY):
-        categories.append(stel_dataset.output['style_type'][i * NUM_ROWS_PER_CATEGORY] + ' ' + stel_dataset.output['language'][i * NUM_ROWS_PER_CATEGORY])
+        categories.append(
+            stel_dataset.output['style_type'][i * NUM_ROWS_PER_CATEGORY]
+            + ' '
+            + stel_dataset.output['language'][i * NUM_ROWS_PER_CATEGORY]
+        )
     return categories
 
-def STEL_table(model, type='STEL'):
-    accuracies, avg_accuracy = STEL_benchmark(stel_dataset.output['positive'], stel_dataset.output['negative'], model, type)
+
+def STEL_table(model_name, model=None, type='STEL'):
+    if not model:
+        model = SentenceTransformersEmbedder(model_name=model)
+
+    accuracies, avg_accuracy = STEL_benchmark(
+        stel_dataset.output['positive'],
+        stel_dataset.output['negative'],
+        model_name,
+        model,
+        type,
+    )
     accuracies.append(avg_accuracy)
     categories = STEL_categories()
     categories.append('average')
-    data = {
-        'Metric': categories,
-        f'{model} Embeddings': accuracies
-    }
+    data = {'Metric': categories, f'{model_name} Embeddings': accuracies}
     df = pd.DataFrame(data)
     return df
+
 
 def merge_dfs(dfs):
     for df in dfs:
@@ -152,8 +226,27 @@ def merge_dfs(dfs):
     return merged_df
 
 
-tpe = 'STEL-or-content'
-models = ['AnnaWegmann/Style-Embedding', 'SynthSTEL/styledistance', 'SynthSTEL/styledistance_synthetic_only', 'google-bert/bert-base-cased', 'FacebookAI/roberta-base']
-tables = [STEL_table(model, type=tpe) for model in models]
+tpe = 'STEL'
+models = [
+    ('Wegmann', SentenceTransformersEmbedder(model_name='AnnaWegmann/Style-Embedding')),
+    (
+        'StyleDistance',
+        SentenceTransformersEmbedder(model_name='SynthSTEL/styledistance'),
+    ),
+    (
+        'StyleDistance Synthetic Only',
+        SentenceTransformersEmbedder(
+            model_name='SynthSTEL/styledistance_synthetic_only'
+        ),
+    ),
+    ('bert-base-cased', SentenceTransformersEmbedder('google-bert/bert-base-cased')),
+    ('roberta-base', SentenceTransformersEmbedder('FacebookAI/roberta-base')),
+    ('xlm-roberta-base', SentenceTransformersEmbedder('FacebookAI/xlm-roberta-base')),
+    ('lisa', None),
+    ('luar', None),
+]
+tables = [STEL_table(name, model, type=tpe) for name, model in models]
 merged_dfs = merge_dfs(tables)
 print(merged_dfs.to_markdown())
+output_file = f'{tpe}.xlsx'
+merged_dfs.to_excel(output_file, index=False)
--- multilingual_dataset_script.py
+++ multilingual_dataset_script.py
@@ -2,171 +2,208 @@ import pandas as pd
 import os
 import glob
 from datasets import load_dataset
-from sklearn.model_selection import train_test_split
 from datasets import DatasetDict, Dataset
+import itertools
+
+language_map = {
+    'German': 'de',
+    'german': 'de',
+    'English': 'en',
+    'english': 'en',
+    'refined-english': 'en',
+    'Refined-English': 'en',
+    'French': 'fr',
+    'french': 'fr',
+    'Italian': 'it',
+    'italian': 'it',
+    'Japanese': 'ja',
+    'japanese': 'ja',
+    'Brazilian Portuguese': 'pt-br',
+    'brazilian portuguese': 'pt-br',
+    'brazilian_portuguese': 'pt-br',
+    'Brazilian_Portuguese': 'pt-br',
+    'Russian': 'ru',
+    'russian': 'ru',
+    'Slovene': 'sl',
+    'slovene': 'sl',
+    'Spanish': 'es',
+    'spanish': 'es',
+    'Hindi': 'hi',
+    'hindi': 'hi',
+    'Ukrainian': 'uk',
+    'ukrainian': 'uk',
+    'Chinese': 'zh',
+    'chinese': 'zh',
+    'Bengali': 'bn',
+    'bengali': 'bn',
+    'Magahi': 'mag',
+    'magahi': 'mag',
+    'Malayalam': 'ml',
+    'malayalam': 'ml',
+    'Marathi': 'mr',
+    'marathi': 'mr',
+    'Odia': 'or',
+    'odia': 'or',
+    'Punjabi': 'pa',
+    'punjabi': 'pa',
+    'Telugu': 'te',
+    'telugu': 'te',
+    'Urdu': 'ur',
+    'urdu': 'ur',
+    'Amharic': 'am',
+    'amharic': 'am',
+    'Arabic': 'ar',
+    'arabic': 'ar',
+    'am': 'am',
+    'ru': 'ru',
+    'hi': 'hi',
+    'en': 'en',
+    'uk': 'uk',
+    'ar': 'ar',
+    'es': 'es',
+    'zh': 'zh',
+    'de': 'de'
+}
+
+def get_paired_tsv(df):
+    pairwise_combinations = list(itertools.combinations(df.index, 2))
+    pairwise_df = pd.DataFrame({
+        'anchor_1': [df.loc[i, 'positive'] for i, j in pairwise_combinations],
+        'alternative_1': [df.loc[i, 'negative'] for i, j in pairwise_combinations],
+        'anchor_2': [df.loc[j, 'positive'] for i, j in pairwise_combinations],
+        'alternative_2': [df.loc[j, 'negative'] for i, j in pairwise_combinations],
+    })
+    return pairwise_df
+
 
 def convert_complex_multisim():
-    root_folder = 'multilingual_raw/MultiSim/data'
+    languages = ['English', 'Italian', 'French', 'Japanese', 'Brazilian Portuguese', 'German', 'Russian', 'Slovene']
     output_folder = 'multilingual_raw_output/complex'
+    pairwise_output_folder = 'pairwise_multilingual_raw_output/complex'
 
-    for language_folder in os.listdir(root_folder):
-        language_path = os.path.join(root_folder, language_folder)
-        if os.path.isdir(language_path):
-            csv_files = glob.glob(os.path.join(language_path, '*_train.csv'))
-            for csv_file in csv_files:
-                tsv_file = os.path.join(output_folder, f'{language_folder.lower()}_complex.tsv')
-                try:
-                    df = pd.read_csv(csv_file)
-                    if len(df) < 100 * 2:
-                        raise ValueError("Not enough rows to sample 100 pairs.")
-                    
-                    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-                    df_sampled = df.iloc[:100*2]
-                    rows = []
-                    for i in range(100):
-                        anchor1 = df_sampled.iloc[i]['original']
-                        anchor2 = df_sampled.iloc[i]['simple']
-                        rows.append([anchor1, anchor2])
-                    
-                    tsv_df = pd.DataFrame(rows, columns=['positive', 'negative'])
-                    tsv_df['style_type'] = 'simplicity'
-                    tsv_df['language'] = language_folder
-                    tsv_df.to_csv(tsv_file, sep='\t', index=False)
-                except ValueError as e:
-                    print(f"Skipping {csv_file}: {e}")
+    for lang in languages:
+        ds = load_dataset('MichaelR207/MultiSim', lang, trust_remote_code=True)
+        tsv_file = os.path.join(output_folder, f'{language_map[lang]}.tsv')
+        pairwise_tsv_file = os.path.join(pairwise_output_folder, f'{language_map[lang]}.tsv')
+        rows = []
+        for i in range(100):
+            anchor1 = ds['train'][i]['original']
+            anchor2 = ds['train'][i]['simple']['simplifications'][0]
+            rows.append([anchor1, anchor2])
+        df = pd.DataFrame(rows, columns=['positive', 'negative'])
+        df['style_type'] = 'simplicity'
+        df['language'] = language_map[lang]
+        df.to_csv(tsv_file, sep='\t', index=False)
+
+        pairwise_df = get_paired_tsv(df)
+        pairwise_df['style_type'] = 'simplicity'
+        pairwise_df['language'] = language_map[lang]
+        pairwise_df.to_csv(pairwise_tsv_file, sep='\t', index=False)
+        print(f'TSV file saved for {lang} at {tsv_file}')
 
 def convert_formal_xformal():
-    def read_file_lines(file_path):
-        with open(file_path, 'r', encoding='utf-8') as f:
-            return [line.strip() for line in f.readlines()]
+    root_folder = 'multilingual_raw/XFormal'
+    output_folder = 'multilingual_raw_output/formal'
+    pairwise_output_folder = 'pairwise_multilingual_raw_output/formal'
 
-    def create_tsv_for_each_language(base_dir, num_samples=100):
-        languages = os.listdir(base_dir)
+    for lang in os.listdir(root_folder):
+        formal_file = os.path.join(root_folder, lang, 'formal0')
+        informal_file = os.path.join(root_folder, lang, 'informal')
+        tsv_file = os.path.join(output_folder, f'{language_map[lang.lower()]}.tsv')
+        pairwise_tsv_file = os.path.join(pairwise_output_folder, f'{language_map[lang.lower()]}.tsv')
+        if not os.path.exists(formal_file) or not os.path.exists(informal_file):
+            print(f'Warning: Missing files for {lang}!')
+            continue
+        with open(formal_file, 'r', encoding='utf-8') as f:
+            formal_lines = [line.strip() for line in f.readlines()]
+        with open(informal_file, 'r', encoding='utf-8') as f:
+            informal_lines = [line.strip() for line in f.readlines()]
 
-        for lang in languages:
-            formal_file = os.path.join(base_dir, lang, 'formal0')
-            informal_file = os.path.join(base_dir, lang, 'informal')
-            
-            if not os.path.exists(formal_file) or not os.path.exists(informal_file):
-                print(f"Warning: Missing files for {lang}!")
-                continue
+        rows = []
+        for i in range(100):
+            anchor1 = formal_lines[i]
+            anchor2 = informal_lines[i]
+            rows.append([anchor1, anchor2])
 
-            formal_lines = read_file_lines(formal_file)
-            informal_lines = read_file_lines(informal_file)
+        df = pd.DataFrame(rows, columns=['positive', 'negative'])
+        df['style_type'] = 'formality'
+        df['language'] = language_map[lang.lower()]
+        df.to_csv(tsv_file, sep='\t', index=False)
 
-            data = {
-                "positive": [],
-                "negative": [],
-            }
+        pairwise_df = get_paired_tsv(df)
+        pairwise_df['style_type'] = 'formality'
+        pairwise_df['language'] = language_map[lang]
+        pairwise_df.to_csv(pairwise_tsv_file, sep='\t', index=False)
+        print(f'TSV file saved for {lang} at {tsv_file}')
 
-            for i in range(100):
-                anchor1 = formal_lines[i]
-                anchor2 = informal_lines[i]
-                data["positive"].append(anchor1)
-                data["negative"].append(anchor2)
-
-            df = pd.DataFrame(data, columns=['positive', 'negative'])
-            df['style_type'] = 'formal'
-            df['language'] = lang.lower()
-
-            output_file = f'multilingual_raw_output/formal/{lang.lower()}_formal.tsv'
-            df.to_csv(output_file, sep='\t', index=False)
-            print(f"TSV file saved for {lang} at {output_file}")
-
-    base_dir = 'multilingual_raw/XFormal'
-    create_tsv_for_each_language(base_dir, num_samples=100)
 
 def convert_toxic_paradetox():
-    ds = load_dataset("textdetox/multilingual_paradetox")
-
-    def sample_and_create_tsv(dataset, language, num_samples=100):
-        """Sample the dataset and create a TSV file for each language."""
-        data = {
-            "positive": [],
-            "negative": [],
-        }        
+    dataset = load_dataset('textdetox/multilingual_paradetox', trust_remote_code=True)
+    output_folder = 'multilingual_raw_output/toxic'
+    pairwise_output_folder = 'pairwise_multilingual_raw_output/toxic'
+    for lang in dataset:
+        tsv_file = os.path.join(output_folder, f'{language_map[lang.lower()]}.tsv')
+        pairwise_tsv_file = os.path.join(pairwise_output_folder, f'{language_map[lang.lower()]}.tsv')
+        data = []
         for i in range(100):
-            anchor1 = dataset[i]['toxic_sentence']
-            anchor2 = dataset[i]['neutral_sentence']
-            data["positive"].append(anchor1)
-            data["negative"].append(anchor2)
+            anchor1 = dataset[lang][i]['toxic_sentence']
+            anchor2 = dataset[lang][i]['neutral_sentence']
+            data.append([anchor1, anchor2])
 
-        # Create a pandas DataFrame
         df = pd.DataFrame(data, columns=['positive', 'negative'])
-        df['style_type'] = 'toxic'
-        df['language'] = lang.lower()
+        df['style_type'] = 'toxicity'
+        df['language'] = language_map[lang.lower()]
+        df.to_csv(tsv_file, sep='\t', index=False)
 
-        # Write the DataFrame to a TSV file for the specific language
-        output_file = f'multilingual_raw_output/toxic/{language}_toxic.tsv'  # File name based on language
-        df.to_csv(output_file, sep='\t', index=False)
-        print(f"TSV file saved for {language} at {output_file}")
-
-    # Loop through each language in the dataset and create TSV files
-    for lang in ds:
-        sample_and_create_tsv(ds[lang], lang, num_samples=100)
+        pairwise_df = get_paired_tsv(df)
+        pairwise_df['style_type'] = 'toxicity'
+        pairwise_df['language'] = language_map[lang.lower()]
+        pairwise_df.to_csv(pairwise_tsv_file, sep='\t', index=False)
+        print(f'TSV file saved for {lang} at {output_folder}')
 
 def convert_positive_indian():
-    output_dir = 'multilingual_raw_output/positive'
-    def sample_and_create_tsv(input_csv, output_tsv, lang, num_samples=100):
-        """Convert a CSV into a TSV with sampled anchor and alternative pairs."""
-        df = pd.read_csv(input_csv, delimiter=',')
-        data = {
-            "positive": [],
-            "negative": [],
-        }
+    base_dir = 'multilingual_raw/multilingual-tst-datasets'
+    output_folder = 'multilingual_raw_output/positive'
+    pairwise_output_folder = 'pairwise_multilingual_raw_output/positive'
+
+    for lang in os.listdir(base_dir):
+        lang_dir = os.path.join(base_dir, lang)
+        csv_files = [f for f in os.listdir(lang_dir) if f.endswith('.csv')]
+        if not csv_files:
+            continue
+        input_csv = os.path.join(lang_dir, csv_files[0])
+        tsv_file = os.path.join(output_folder, f'{language_map[lang.lower()]}.tsv')
+        pairwise_tsv_file = os.path.join(pairwise_output_folder, f'{language_map[lang.lower()]}.tsv')
+        csv_data = pd.read_csv(input_csv, delimiter=',')
+        data = []
         for i in range(100):
-            anchor1 = df.iloc[i]["NEGATIVE"]
-            anchor2 = df.iloc[i]["POSITIVE"]
-            data["positive"].append(anchor1)
-            data["negative"].append(anchor2)
+            anchor1 = csv_data.iloc[i]['NEGATIVE']
+            anchor2 = csv_data.iloc[i]['POSITIVE']
+            data.append([anchor1, anchor2])
 
-        output_df = pd.DataFrame(data, columns=['positive', 'negative'])
-        output_df['style_type'] = 'positivity'
-        output_df['language'] = lang
-        output_df.to_csv(output_tsv, sep='\t', index=False)
-        print(f"TSV file saved at {output_tsv}")
+        df = pd.DataFrame(data, columns=['positive', 'negative'])
+        df['style_type'] = 'positivity'
+        df['language'] = language_map[lang]
+        df.to_csv(tsv_file, sep='\t', index=False)
 
-    def process_multilingual_datasets(base_dir):
-        for lang in os.listdir(base_dir):
-            lang_dir = os.path.join(base_dir, lang)
-            try:
-                csv_files = [f for f in os.listdir(lang_dir) if f.endswith(".csv")]
-                input_csv = os.path.join(lang_dir, csv_files[0])
-                output_tsv = os.path.join(output_dir, f"{lang}_positive.tsv")
-
-                sample_and_create_tsv(input_csv, output_tsv, lang)
-            except:
-                pass
-    # Define the base directory containing language folders
-    base_dir = "multilingual_raw/multilingual-tst-datasets"
-
-    # Process all language folders
-    process_multilingual_datasets(base_dir)
-
-
-convert_complex_multisim()
-convert_formal_xformal()
-convert_toxic_paradetox()
-convert_positive_indian()
+        pairwise_df = get_paired_tsv(df)
+        pairwise_df['style_type'] = 'positivity'
+        pairwise_df['language'] = language_map[lang.lower()]
+        pairwise_df.to_csv(pairwise_tsv_file, sep='\t', index=False)
+        print(f'TSV file saved for {lang} at {tsv_file}')
 
 def merge_csvs():
-    # Step 1: Load and concatenate all CSVs
-    all_files = glob.glob("multilingual_raw_output/*/*.tsv")
+    all_files = glob.glob('multilingual_raw_output/*/*.tsv')
     df = pd.concat((pd.read_csv(file, sep='\t') for file in all_files), ignore_index=True)
-
-    # Step 2: Create train/test split
-    # train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
-
-    # Step 3: Convert to Hugging Face Dataset
-    # train_dataset = Dataset.from_pandas(train_df)
-    # test_dataset = Dataset.from_pandas(test_df)
-
-    # Step 4: Combine and push to Hugging Face
     dataset = DatasetDict({
-        "data": Dataset.from_pandas(df)
-        # "train": train_dataset,
-        # "test": test_dataset
+        'train': Dataset.from_pandas(df)
     })
-    dataset.push_to_hub("multilingual_stel_test")
+    return dataset
 
-merge_csvs()
+# convert_complex_multisim()
+# convert_formal_xformal()
+# convert_toxic_paradetox()
+# convert_positive_indian()
+dataset = merge_csvs()
+dataset.push_to_hub('StyleDistance/multilingual_stel')
+dataset.push_to_hub('multilingual_stel')
